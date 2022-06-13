19f600aeb5fab31e545d5cbafd1404742a37732e
OpenSauce <lawleycobhc@gmail.com>
2022-06-13 19:04:52 +0100

    And a little more

--- paths_test.go
+++ paths_test.go
@@ -22,8 +22,8 @@ func BenchmarkGetPathFromCellsA(b *testing.B) {
 
 func TestGetPathFromCellsA(t *testing.T) {
 	firstMap := NewGrid(200, 200, 16, 16)
-	p1 := firstMap.GetPathFromCells(firstMap.Get(0, 0), firstMap.Get(10, 5), true, true)
-	p2 := firstMap.GetPathFromCellsAStar(firstMap.Get(0, 0), firstMap.Get(10, 5), true, true)
+	p1 := firstMap.GetPathFromCells(firstMap.Get(0, 0), firstMap.Get(199, 5), true, true)
+	p2 := firstMap.GetPathFromCellsAStar(firstMap.Get(0, 0), firstMap.Get(199, 5), true, true)
 	if !p1.Same(p2) {
 		t.Fatalf("Path 1 %f:\n%s\nPath 2 %f:\n%s", p1.TotalCost(), p1, p2.TotalCost(), p2)
 	}
