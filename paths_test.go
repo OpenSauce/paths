13bace70fd51f55b135c9a9019a71711e4bbd705
Leigh <lawleycobhc@gmail.com>
2022-06-15 09:20:25 +0100

    A Star  Algorithm Implementation (#4)
    * Started working on basic A star
    
    * Worked on A* a little more
    
    * And a little more
    
    * Worked on A* algorithm a little bit
    
    * Working version of A*
    
    * Changed the pathing method to A*
    
    * Update paths.go

--- paths_test.go
+++ paths_test.go
@@ -7,23 +7,23 @@ import (
 var p *Path
 
 func BenchmarkGetPathFromCells(b *testing.B) {
-	firstMap := NewGrid(200, 200, 16, 16)
+	firstMap := NewGrid(500, 500, 16, 16)
 	for i := 0; i < b.N; i++ {
-		p = firstMap.GetPathFromCells(firstMap.Get(0, 0), firstMap.Get(10, 5), true, true)
+		p = firstMap.GetPathFromCells(firstMap.Get(0, 0), firstMap.Get(0, 499), true, true)
 	}
 }
 
 func BenchmarkGetPathFromCellsA(b *testing.B) {
-	firstMap := NewGrid(200, 200, 16, 16)
+	firstMap := NewGrid(500, 500, 16, 16)
 	for i := 0; i < b.N; i++ {
-		p = firstMap.GetPathFromCellsAStar(firstMap.Get(0, 0), firstMap.Get(10, 5), true, true)
+		p = firstMap.GetPathFromCellsAStar(firstMap.Get(0, 0), firstMap.Get(0, 499), true, true)
 	}
 }
 
 func TestGetPathFromCellsA(t *testing.T) {
 	firstMap := NewGrid(200, 200, 16, 16)
-	p1 := firstMap.GetPathFromCells(firstMap.Get(0, 0), firstMap.Get(199, 5), true, true)
-	p2 := firstMap.GetPathFromCellsAStar(firstMap.Get(0, 0), firstMap.Get(199, 5), true, true)
+	p1 := firstMap.GetPathFromCells(firstMap.Get(0, 0), firstMap.Get(199, 199), true, true)
+	p2 := firstMap.GetPathFromCellsAStar(firstMap.Get(0, 0), firstMap.Get(199, 199), true, true)
 	if !p1.Same(p2) {
 		t.Fatalf("Path 1 %f:\n%s\nPath 2 %f:\n%s", p1.TotalCost(), p1, p2.TotalCost(), p2)
 	}
